32bf9d7738438096d2012e4b323ac8958fbb91af
ereio <ereio@protonmail.com>
2018-03-01 11:29:10 -0500

    adding model for movie

--- lib/model/Movie.dart
+++ lib/model/Movie.dart
@@ -1,22 +1,22 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
 
-class Movie {
-  String title;
-  String author;
-  String director;
-  String posterUrl;
-  String description;
+  class Movie {
+    String title;
+    String author;
+    String director;
+    String posterUrl;
+    String description;
 
-   Movie(String title, String description) {
-     this.title = title;
-     this.description = description;
-   }
+     Movie(String title, String description) {
+       this.title = title;
+       this.description = description;
+     }
 
-   Movie.fromJson(Map<String, String> jsonObj) {
-    this.title = jsonObj['title'];
-    this.author = jsonObj['author'];
-    this.director = jsonObj['director'];
-    this.posterUrl = jsonObj['posterUrl'];
-    this.description = jsonObj['description'];
+     Movie.fromJson(Map<String, String> jsonObj) {
+      this.title = jsonObj['title'];
+      this.author = jsonObj['author'];
+      this.director = jsonObj['director'];
+      this.posterUrl = jsonObj['posterUrl'];
+      this.description = jsonObj['description'];
+    }
   }
-}
